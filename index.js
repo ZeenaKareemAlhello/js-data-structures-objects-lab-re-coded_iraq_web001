2577d05d1932c54e6f91a31d85966cede920cf65
zena abd alkarem alhello <zena34269@gmail.com>
2020-08-21 11:53:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 let driver={};
 function updateDriverWithKeyAndValue(driverObj ,key ,value)
 {  let driverNew={...driverObj} 
@@ -12,3 +13,7 @@ function deleteFromDriverByKey(driverObj,key){
   return driverNew;}
   function destructivelyDeleteFromDriverByKey(driverObj,key){  delete driverObj[key]; 
   return driverObj;}
+=======
+let driver={};function updateDriverWithKeyAndValue(driverObj ,key ,value){  let driverNew={...driverObj}  driverNew[key]=value;  return driverNew;}
+function destructivelyUpdateDriverWithKeyAndValue(driverObj ,key ,value){    driverObj[key]=value;  return driverObj;}function deleteFromDriverByKey(driverObj,key){  let driverNew = Object.assign({},driverObj);  delete driverNew[key];  return driverNew;}function destructivelyDeleteFromDriverByKey(driverObj,key){  delete driverObj[key];  return driverObj;}
+>>>>>>> 48d409883cc4fd90a355a0474b94cf36d71de06d
